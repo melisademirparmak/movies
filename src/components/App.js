7e53e58dc8334d2c3853f313d14d551059187e1e
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-07 14:02:05 +0300

    search enabled

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MovieList from "./MovieList";
+import SearchBar from "./SerachBar";
 
 class App extends React.Component {
   state = {
@@ -8,41 +9,92 @@ class App extends React.Component {
         id: 1,
         name: "The Flash",
         rating: 8.3,
-        overwiev:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, error! ",
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
         imageURL:
-          "https://d1nslcd7m2225b.cloudfront.net/Pictures/480xAny/8/0/3/1206803_Intersetellar.jpg",
+          "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg",
       },
+
       {
         id: 2,
         name: "Interstellar",
         rating: 6.8,
-        overwiev:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, error! ",
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
         imageURL:
-          "https://d1nslcd7m2225b.cloudfront.net/Pictures/480xAny/8/0/3/1206803_Intersetellar.jpg",
+          "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+      },
+
+      {
+        id: 3,
+        name: "Arrow",
+        rating: 7.9,
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        imageURL:
+          "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg",
+      },
+      {
+        id: 4,
+        name: "Rogue",
+        rating: 7.4,
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        imageURL:
+          "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+      },
+
+      {
+        id: 5,
+        name: "Project Power",
+        rating: 6.7,
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        imageURL:
+          "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg",
+      },
+
+      {
+        id: 6,
+        name: "Superman",
+        rating: 7.6,
+        overview:
+          "This is a wider card with supporting text below as a natural lead-in to additional content.",
+        imageURL:
+          "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg",
       },
     ],
+    searchQuery: "",
   };
 
   deleteMovie = (movie) => {
     const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
-    this.setState({
+    this.setState((state) => ({
       movies: newMovieList,
-    });
+    }));
+  };
+
+  searchMovie = (event) => {
+    this.setState({ searchQuery: event.target.value });
   };
 
   render() {
+    let filteredMovies = this.state.movies.filter((movie) => {
+      return (
+        movie.name
+          .toLowerCase()
+          .indexOf(this.state.searchQuery.toLowerCase()) !== -1
+      );
+    });
     return (
       <div className="container">
         <div className="row">
-          <div className="col-lg-12"></div>
+          <div className="col-lg-12">
+            <SearchBar searchMovieProp={this.searchMovie} />
+          </div>
         </div>
-        <MovieList
-          movies={this.state.movies}
-          deleteMovieProp={this.deleteMovie}
-        />
+        <MovieList movies={filteredMovies} deleteMovieProp={this.deleteMovie} />
       </div>
     );
   }
--- src/components/SerachBar.js
+++ src/components/SerachBar.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   render() {
-    return <h2>SeracBar</h2>;
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <div className="form-row mb-5 mt-5">
+          <div className="col">
+            <input
+              onChange={this.props.searchMovieProp}
+              type="text"
+              className="form-control"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+      </form>
+    );
   }
 }
 
